e7c057be0075c7ba2f2b4933c70bbbc0b32d5ba0
Kunal <kunalsheth1177@gmail.com>
2022-08-16 13:52:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/reports' exact element={<Reports/>} />
+          <Route path='/products' exact element={<Products/>} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
